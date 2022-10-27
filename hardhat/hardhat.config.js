fb1a82a513906671c7ca710f6f6377b96094c9e5
David Milai <davidmilai67@gmail.com>
2022-10-27 04:54:08 +0300

    hardhat config

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,20 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+
+networks:{
+  goerli: {
+    url: ALCHEMY_API_KEY_URL,
+    accounts: [PRIVATE_KEY],
+  },
+} 
+
 };
---
+++ hardhat/scripts/deployTwo.js
@@ -0,0 +1,24 @@
+
+
+// const {ethers} = require("hardhat");
+
+// async function main() {
+
+//     //find the contract to be deployed
+//     const contract = await ethers.getContractFactory("NFT");
+    
+//     //deploy contract
+//     const deployedContract = await contract.deploy();
+
+//     //wait for deployment to finish
+//     await deployedContract.deployed();
+
+//     console.log("contract is deployed to", deployedContract.address);
+// }
+
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
