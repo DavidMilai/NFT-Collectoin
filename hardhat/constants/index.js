ca1f7f97ed6039810bf97e1bdc8960121a888c13
David Milai <davidmilai67@gmail.com>
2022-11-04 05:49:21 +0300

    created new contract

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,6 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x7413B36889168ac5596eC688cE51e36A5D38Fd5E";
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collectoin.vercel.app/api/";
 
 
 module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
