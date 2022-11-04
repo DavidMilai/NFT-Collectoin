2fc095f2a7ce273a7327d77a8b0a033afb877778
David Milai <davidmilai67@gmail.com>
2022-11-04 05:06:40 +0300

    string concatination

--- nft-frontend/pages/api/[tokenId].js
+++ nft-frontend/pages/api/[tokenId].js
@@ -1,18 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const tokenId = req.query.tokenId;
+  const tokenId = req.query.tokenId; 
 
-  const name = "Crypto dev #${tokenId}";
+  const name = "Crypto dev #"+tokenId;
 
   const description = "CryptoDevs is an NFT collection woe Web3 Devs";
 
-  const image = "https://github.com/LearnWeb3DAO/NFT-Collection/blob/main/my-app/public/cryptodevs/${Number(tokenId) - 1}.svg";
+  const image = "https://github.com/LearnWeb3DAO/NFT-Collection/blob/main/my-app/public/cryptodevs/"+tokenId;
 
 
   return res.json({
     name: name,
     description: description,
-    image: image,
+    image: image+".svg",
   });
 }
